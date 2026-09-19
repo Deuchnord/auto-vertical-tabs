dc00d61deedf4e5c6f9fdb5d788348857f61d740
Deuchnord <git@deuchnord.fr>
2026-09-19 09:40:02 +0200

    feat: add translations for French and German

--- settings/settings.js
+++ settings/settings.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Internationalization:
+    console.log(browser.i18n.getMessage('prefMinNbTabsLabel'))
+    document.getElementById('label-minNbTabs').innerText = browser.i18n.getMessage('prefMinNbTabsLabel');
+    document.getElementById('save-settings').innerText = browser.i18n.getMessage('prefApplyButton');
+
     const minNbTabs = document.getElementById('minNbTabs');
 
     browser.storage.sync.get('minNbTabs').then(function (settings) {
@@ -7,5 +12,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('save-settings').addEventListener('click', function () {
         browser.storage.sync.set({ minNbTabs: minNbTabs.value });
-    })
+    });
 });
